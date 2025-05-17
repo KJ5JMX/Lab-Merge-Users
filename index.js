0a183527bcafa3af1446dd24c24821a3cfb1f6e7
Blake Godsey <somethingclever182@gmail.com>
2025-05-17 02:01:47 -0500

    merging companies into one and marked the date that they were merged

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+require(Date.js)
+
+
+
+//combine the users 
+function combineUsers(...args) {
+ const users = [];
+
+ args.forEach(arr => {
+  users.push(...arr);//spread each array into user list. 
+ });
+  return {
+    users,
+    merge_date: Date.today().toString("M/d/yyyy")
+    
+  };
+}
+
 
 
 
